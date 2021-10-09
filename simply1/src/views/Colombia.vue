8a1039d4f00620312a084f8fc4bfba9960266eac
Jlrassa <jlrassa@gmail.com>
2021-10-09 01:29:32 -0500

    Update Colombia.vue

--- simply1/src/views/Colombia.vue
+++ simply1/src/views/Colombia.vue
@@ -1,3 +1,42 @@
 <template>
-    <h1>COLOMBIA</h1>
+<v-container>
+  <v-layout wrap class="brown lighten-4">
+    <v-flex class="brown text-center white--text" xs12>
+      <h1>EL CAFÉ EN COLOMBIA</h1>
+    </v-flex>
+
+    <v-flex xs6 align-self-center>
+      <img
+        src="../img/infografia_colombia.png" alt="info_colombia" width="450"
+      />
+    </v-flex>
+
+    <v-flex xs5 align-self-center brown--text>
+      <p>
+        La llegada del café a Colombia se da gracias a los franceses, quienes
+        trajeron la bebida de Europa hacia tierras americanas. <br>Dentro de esta
+        expansión el cafeto llega a las montañas colombianas, donde gracias al
+        clima y a la adaptabilidad de la planta a la tierra se empieza a
+        cultivar y expandir como uno de los productos de mayor expansión dentro
+        del campesinado colombiano. <br>La expansión dentro del territorio nacional
+        se da gracias a los Jesuitas, quienes implantaron el cultivo del cafeto
+        como parte de sus penitencias y logró que la bebida se expandiera
+        inicialmente en la región oriental del país, expandiéndose luego a la
+        zona montañosa andina de Cundinamarca, Antioquia y Caldas. <br>Debido al
+        ataque de la roya a los cultivos asiáticos a mediados del siglo XIX el
+        café colombiano empezó a tener mayor demanda, tanto a nivel local como
+        internacional, lo que hizo que hacia 1870 la producción se multiplicara
+        hasta por 10 veces en un corto periodo y colocarse como el cultivo de
+        mayor reconocimiento del país a nivel mundial. <br>A finales de la década de
+        1920 se crea la Federación Nacional de Cafeteros con el fin de darle
+        mayor apoyo e la producción y comercio del grano y plantear estrategias
+        encaminadas al reconocimiento del cultivo a nivel internacional. Con el
+        tiempo se trabajó en tener mayor reconocimiento y es así como en 1959 se
+        crea el personaje de Juan Valdez como imagen del caficultor colombiano y
+        en la década de 198 se crea el logo y marca de Café de Colombia para
+        darle el sello al producto como el de mayor representatividad del país.
+      </p>
+    </v-flex>
+  </v-layout>
+  </v-container>
 </template>
